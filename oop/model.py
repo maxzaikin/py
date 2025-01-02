eed7af6fc1d7292fc56a95cf3e26a504f04cc592
Maks Zaikin <Max.V.Zaikin@gmail.com>
2025-01-02 16:19:27 +1100

    feat: ch07 changes

--- oop/model.py
+++ oop/model.py
@@ -1,351 +1,64 @@
 """
 Python 3 Object-Oriented Programming Case Study
 
-Chapter 4, Expecting the Unexpected
+Chapter 7.
 """
 from __future__ import annotations
 import collections
-import datetime
-from typing import (
-    cast,
-    Optional,
-    Union,
-    Iterable,
-    Counter,
-    Protocol,
-)
+from dataclasses import dataclass, asdict
+from typing import Optional, Counter, List
 import weakref
+import sys
 
 
-class InvalidSampleError(ValueError):
-    """Source data file has invalid data representation"""
-
-
+@dataclass
 class Sample:
-    """Abstract superclass for all samples."""
-
-    def __init__(
-        self,
-        sepal_length: float,
-        sepal_width: float,
-        petal_length: float,
-        petal_width: float,
-    ) -> None:
-        self.sepal_length = sepal_length
-        self.sepal_width = sepal_width
-        self.petal_length = petal_length
-        self.petal_width = petal_width
-
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f")"
-        )
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
 
 
+@dataclass
 class KnownSample(Sample):
-    """Abstract superclass for testing and training data, the species is set externally."""
-
-    def __init__(
-        self,
-        species: str,
-        sepal_length: float,
-        sepal_width: float,
-        petal_length: float,
-        petal_width: float,
-    ) -> None:
-        super().__init__(
-            sepal_length=sepal_length,
-            sepal_width=sepal_width,
-            petal_length=petal_length,
-            petal_width=petal_width,
-        )
-        self.species = species
-
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f"species={self.species!r}, "
-            f")"
-        )
-
-    @classmethod
-    def from_dict(cls, row: dict[str, str]) -> "KnownSample":
-        if row["species"] not in {"Iris-setosa", "Iris-versicolour", "Iris-virginica"}:
-            raise InvalidSampleError(f"invalid species in {row!r}")
-        try:
-            return cls(
-                species=row["species"],
-                sepal_length=float(row["sepal_length"]),
-                sepal_width=float(row["sepal_width"]),
-                petal_length=float(row["petal_length"]),
-                petal_width=float(row["petal_width"]),
-            )
-        except ValueError as ex:
-            raise InvalidSampleError(f"invalid {row!r}")
+    species: str
 
 
-class TrainingKnownSample(KnownSample):
-    """Training data."""
-
-    @classmethod
-    def from_dict(cls, row: dict[str, str]) -> "TrainingKnownSample":
-        return cast(TrainingKnownSample, super().from_dict(row))
-
-
+@dataclass
 class TestingKnownSample(KnownSample):
-    """Testing data. A classifier can assign a species, which may or may not be correct."""
+    classification: Optional[str] = None
 
-    def __init__(
-        self,
-        /,
-        species: str,
-        sepal_length: float,
-        sepal_width: float,
-        petal_length: float,
-        petal_width: float,
-        classification: Optional[str] = None,
-    ) -> None:
-        super().__init__(
-            species=species,
-            sepal_length=sepal_length,
-            sepal_width=sepal_width,
-            petal_length=petal_length,
-            petal_width=petal_width,
-        )
-        self.classification = classification
 
-    def matches(self) -> bool:
-        return self.species == self.classification
+@dataclass
+class TrainingKnownSample(KnownSample):
+    """Cannot be classified -- there's no classification instance variable available."""
 
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f"species={self.species!r}, "
-            f"classification={self.classification!r}, "
-            f")"
-        )
+    pass
 
-    @classmethod
-    def from_dict(cls, row: dict[str, str]) -> "TestingKnownSample":
-        return cast(TestingKnownSample, super().from_dict(row))
 
+@dataclass
 class UnknownSample(Sample):
-    """A sample provided by a User, not yet classified."""
-
-    @classmethod
-    def from_dict(cls, row: dict[str, str]) -> "UnknownSample":
-        if set(row.keys()) != {
-            "sepal_length",
-            "sepal_width",
-            "petal_length",
-            "petal_width",
-        }:
-            raise InvalidSampleError(f"invalid fields in {row!r}")
-        try:
-            return cls(
-                sepal_length=float(row["sepal_length"]),
-                sepal_width=float(row["sepal_width"]),
-                petal_length=float(row["petal_length"]),
-                petal_width=float(row["petal_width"]),
-            )
-        except (ValueError, KeyError) as ex:
-            raise InvalidSampleError(f"invalid {row!r}")
-
-class ClassifiedSample(Sample):
-    """Created from a sample provided by a User, and the results of classification."""
-
-    def __init__(self, classification: str, sample: UnknownSample) -> None:
-        super().__init__(
-            sepal_length=sample.sepal_length,
-            sepal_width=sample.sepal_width,
-            petal_length=sample.petal_length,
-            petal_width=sample.petal_width,
-        )
-        self.classification = classification
-
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f"classification={self.classification!r}, "
-            f")"
-        )
+    classification: Optional[str] = None
 
 
 class Distance:
-    """A distance computation"""
+    """Abstact definition of a distance computation"""
 
     def distance(self, s1: Sample, s2: Sample) -> float:
         raise NotImplementedError
 
 
-class Chebyshev(Distance):
-    """
-    Computes the Chebyshev distance between two samples.
-
-    ::
-
-        >>> from math import isclose
-        >>> from model import TrainingKnownSample, UnknownSample, Chebyshev
-
-        >>> s1 = TrainingKnownSample(
-        ...     sepal_length=5.1, sepal_width=3.5, petal_length=1.4, petal_width=0.2, species="Iris-setosa")
-        >>> u = UnknownSample(**{"sepal_length": 7.9, "sepal_width": 3.2, "petal_length": 4.7, "petal_width": 1.4})
-
-        >>> algorithm = Chebyshev()
-        >>> isclose(3.3, algorithm.distance(s1, u))
-        True
-
-    """
-
-    def distance(self, s1: Sample, s2: Sample) -> float:
-        return max(
-            [
-                abs(s1.sepal_length - s2.sepal_length),
-                abs(s1.sepal_width - s2.sepal_width),
-                abs(s1.petal_length - s2.petal_length),
-                abs(s1.petal_width - s2.petal_width),
-            ]
-        )
-
-
-class Minkowski(Distance):
-    """An abstraction to provide a way to implement Manhattan and Euclidean."""
-
-    m: int
-
-    def distance(self, s1: Sample, s2: Sample) -> float:
-        return (
-            sum(
-                [
-                    abs(s1.sepal_length - s2.sepal_length) ** self.m,
-                    abs(s1.sepal_width - s2.sepal_width) ** self.m,
-                    abs(s1.petal_length - s2.petal_length) ** self.m,
-                    abs(s1.petal_width - s2.petal_width) ** self.m,
-                ]
-            )
-            ** (1 / self.m)
-        )
-
-
-class Euclidean(Minkowski):
-    m = 2
-
-
-class Manhattan(Minkowski):
-    m = 1
-
-
-class Sorensen(Distance):
-    def distance(self, s1: Sample, s2: Sample) -> float:
-        return sum(
-            [
-                abs(s1.sepal_length - s2.sepal_length),
-                abs(s1.sepal_width - s2.sepal_width),
-                abs(s1.petal_length - s2.petal_length),
-                abs(s1.petal_width - s2.petal_width),
-            ]
-        ) / sum(
-            [
-                s1.sepal_length + s2.sepal_length,
-                s1.sepal_width + s2.sepal_width,
-                s1.petal_length + s2.petal_length,
-                s1.petal_width + s2.petal_width,
-            ]
-        )
-
-
-class Reduce_Function(Protocol):
-    """Define a callable object with specific parameters."""
-
-    def __call__(self, values: list[float]) -> float:
-        pass
-
-
-class Minkowski_2(Distance):
-    """A generic way to implement Manhattan, Euclidean, and Chebyshev.
-
-    ::
-
-        >>> from math import isclose
-        >>> from model import TrainingKnownSample, UnknownSample, Minkowski_2
-
-        >>> class CD(Minkowski_2):
-        ...     m = 1
-        ...     reduction = max
-
-        >>> s1 = TrainingKnownSample(
-        ...     sepal_length=5.1, sepal_width=3.5, petal_length=1.4, petal_width=0.2, species="Iris-setosa")
-        >>> u = UnknownSample(**{"sepal_length": 7.9, "sepal_width": 3.2, "petal_length": 4.7, "petal_width": 1.4})
-
-        >>> algorithm = CD()
-        >>> isclose(3.3, algorithm.distance(s1, u))
-        True
-
-    """
-
-    m: int
-    reduction: Reduce_Function
-
-    def distance(self, s1: Sample, s2: Sample) -> float:
-        # Required to prevent Python from passing `self` as the first argument.
-        summarize = self.reduction
-        return (
-            summarize(
-                [
-                    abs(s1.sepal_length - s2.sepal_length) ** self.m,
-                    abs(s1.sepal_width - s2.sepal_width) ** self.m,
-                    abs(s1.petal_length - s2.petal_length) ** self.m,
-                    abs(s1.petal_width - s2.petal_width) ** self.m,
-                ]
-            )
-            ** (1 / self.m)
-        )
-
-
+@dataclass
 class Hyperparameter:
-    """A hyperparameter value and the overall quality of the classification."""
+    """A specific tuning parameter set with k and a distance algorithm"""
 
-    def __init__(self, k: int, algorithm: "Distance", training: "TrainingData") -> None:
-        self.k = k
-        self.algorithm = algorithm
-        self.data: weakref.ReferenceType["TrainingData"] = weakref.ref(training)
-        self.quality: float = 0.0
+    k: int
+    algorithm: Distance
+    data: weakref.ReferenceType["TrainingData"]
 
-    def test(self) -> None:
-        """Run the entire test suite."""
-        training_data: Optional["TrainingData"] = self.data()
-        if not training_data:
-            raise RuntimeError("Broken Weak Reference")
-        pass_count, fail_count = 0, 0
-        for sample in training_data.testing:
-            sample.classification = self.classify(sample)
-            if sample.matches():
-                pass_count += 1
-            else:
-                fail_count += 1
-        self.quality = pass_count / (pass_count + fail_count)
-
-    def classify(self, sample: Union[UnknownSample, TestingKnownSample]) -> str:
+    def classify(self, sample: Sample) -> str:
         """The k-NN algorithm"""
-        training_data = self.data()
-        if not training_data:
+        if not (training_data := self.data()):
             raise RuntimeError("No TrainingData object")
         distances: list[tuple[float, TrainingKnownSample]] = sorted(
             (self.algorithm.distance(sample, known), known)
@@ -358,41 +71,8 @@ class Hyperparameter:
         return species
 
 
+@dataclass
 class TrainingData:
-    """A set of training data and testing data with methods to load and test the samples."""
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.uploaded: datetime.datetime
-        self.tested: datetime.datetime
-        self.training: list[TrainingKnownSample] = []
-        self.testing: list[TestingKnownSample] = []
-        self.tuning: list[Hyperparameter] = []
-
-    def load(self, raw_data_iter: Iterable[dict[str, str]]) -> None:
-        """Extract TestingKnownSample and TrainingKnownSample from raw data"""
-        for n, row in enumerate(raw_data_iter):
-            try:
-                if n % 5 == 0:
-                    test = TestingKnownSample.from_dict(row)
-                    self.testing.append(test)
-                else:
-                    train = TrainingKnownSample.from_dict(row)
-                    self.training.append(train)
-            except InvalidSampleError as ex:
-                print(f"Row {n+1}: {ex}")
-                return
-        self.uploaded = datetime.datetime.now(tz=datetime.timezone.utc)
-
-    def test(self, parameter: Hyperparameter) -> None:
-        """Test this hyperparamater value."""
-        parameter.test()
-        self.tuning.append(parameter)
-        self.tested = datetime.datetime.now(tz=datetime.timezone.utc)
-
-    def classify(
-        self, parameter: Hyperparameter, sample: UnknownSample
-    ) -> ClassifiedSample:
-        return ClassifiedSample(
-            classification=parameter.classify(sample), sample=sample
-        )
+    testing: List[TestingKnownSample]
+    training: List[TrainingKnownSample]
+    tuning: List[Hyperparameter]
